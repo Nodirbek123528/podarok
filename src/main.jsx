64cebb84365de477157e6c00179eea3d1522dd19
Nodirbek123528 <nodirbek.08@icloud.com>
2024-03-26 00:48:39 +0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './App.css';
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <div className='button'>
+
+    <img className='img' src="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678087-heart-512.png" alt="" />
+  </div>
+)
