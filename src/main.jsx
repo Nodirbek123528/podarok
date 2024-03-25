0d30616e01badeb0970e5ade8c6f53835ee20cbc
Nodirbek123528 <nodirbek.08@icloud.com>
2024-03-26 00:58:30 +0500

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div className='button'>
+    <h1 className='title'>Нажми на меня</h1>
 
     <img className='img' src="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678087-heart-512.png" alt="" />
   </div>
